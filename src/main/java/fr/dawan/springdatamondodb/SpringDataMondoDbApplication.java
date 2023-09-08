c1496fc388139a47176f64194155ccad59649269
Yanis Adekalom <yanis971.60@gmail.com>
2023-09-08 15:04:17 +0200

    Initial commit

---
+++ src/main/java/fr/dawan/springdatamondodb/SpringDataMondoDbApplication.java
@@ -0,0 +1,13 @@
+package fr.dawan.springdatamondodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataMondoDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataMondoDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/dawan/springdatamondodb/SpringDataMondoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dawan.springdatamondodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataMondoDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
